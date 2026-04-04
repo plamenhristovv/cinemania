ca288c5c837ac1ebce80efe127a87a2fb58ba17c
Plamen Hristov <plamen.kostov.hristov@gmail.com>
2026-04-04 22:52:09 +0300

    chore: adding accounts model

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, UserLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
 from .forms import UserRegistrationForm
 
 class SignUpView(CreateView):
     form_class = UserRegistrationForm
     template_name = 'accounts/signup.html'
     success_url = reverse_lazy('login')
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+class UserLogoutView(LogoutView):
+    pass
